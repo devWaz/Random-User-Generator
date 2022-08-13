5c743ee2ce4cc857a7828b6f8ac61c0c7c27c07f
devWaz <fawazodessy@gmail.com>
2022-08-13 20:17:47 +0100

    FInally fixed this bug 😩�

--- src/Main.js
+++ src/Main.js
@@ -1,32 +1,65 @@
-import { useState, useEffect } from "react";
+import {  useState } from "react";
+import { useEffect } from "react";
+
 
 const Main = () => {
-    const [user , setUser] = useState([]);
+    const [user , setUser] = useState({});
+    const [isLoading , setIsLoading] = useState(true);
+    const [errorMessage , setErrorMessage] = useState(null);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    // const [age, setAge] = useState("");
+    // const [location, setlocation] = useState("");
 
-    const getUser = () => {
-        fetch('https://randomuser.me/api/')
-        .then(res => res.json())
-        .then(data => {
-        setUser(data.results[0]);
-        })
+    const fetchUser = () => {
+      fetch('https://randomuser.me/api')
+      .then(res =>{ 
+        if (!res.ok){
+          throw Error("Could not fetch data");
+          }
+        return res.json();
+      })
+      .then(data => {
+      setUser(data.results[0]);
+      setFirstName(data.results[0].name.first);
+      setLastName(data.results[0].name.last);
+      setIsLoading(false);
+      setErrorMessage(null);
+      })
+      .catch(
+        err => {
+          setIsLoading(false)
+          setErrorMessage(err.message)
+      }
+      )
     }
 
-    useEffect(()=>{getUser()}, [])
+  useEffect(fetchUser, []);
 
-    console.log(user);
+
+console.log(user);
+console.log(isLoading);
+console.log(errorMessage);
     return (
         <div className="main">
-            <div>
-                <div className="profile-card">
-                    {/* <p>First Name: {user.name.first}</p>
-                    <p>Last Name: {user.name.last}</p>
-                    <p>Location: {user.location.city}, {user.location.state}</p> */}
-                </div>
-                <button onClick={getUser}>click</button>
+             <div className="profile">
+              { isLoading && <div>Loading....</div> }
+              { errorMessage && <div>{ errorMessage }</div> }
+              {user &&  
+              <div className="profile-card">
+                      {/* <img src={user.picture.medium} alt=""/> */}
+                      <div>
+                        <p>First Name: {firstName}</p>
+                        <p>Last Name: {lastName}</p>
+                        {/* <p>Location: {user.location.city}, {user.location.state}</p> */}
+                      </div>
+                 </div> }
             </div>
+
+              <button onClick={fetchUser}>New User</button>
         </div>
     );
-}
+ }
 
-export default Main;
+ export default Main;
 
