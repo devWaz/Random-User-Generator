99c47d5dd00fee7f75f728c1c5297337494a982d
devWaz <fawazodessy@gmail.com>
2022-10-20 23:41:26 +0100

    Responsive

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,18 @@
 import {  useState } from "react";
 import { useEffect } from "react";
-// import { Fade } from "react-awesome-reveal";
 
 
 const Main = () => {
-    const [user , setUser] = useState({});
+    const [user , setUser] = useState();
     const [isLoading , setIsLoading] = useState(true);
     const [errorMessage , setErrorMessage] = useState(null);
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [age, setAge] = useState("");
-    const [avatar, setAvatar] = useState("");
-    const [city, setCity] = useState("");
-    const [state, setState] = useState("");
+    // const [firstName, setFirstName] = useState("");
+    // const [lastName, setLastName] = useState("");
+    // const [age, setAge] = useState("");
+    // const [avatar, setAvatar] = useState("");
+    // const [city, setCity] = useState("");
+    // const [state, setState] = useState("");
+    console.log(user);
 
     const fetchUser = () => {
       fetch('https://randomuser.me/api')
@@ -26,12 +26,12 @@ const Main = () => {
       setIsLoading(false);
       setErrorMessage(null);
       setUser(data.results[0]);
-      setFirstName(data.results[0].name.first);
-      setLastName(data.results[0].name.last);
-      setAvatar(data.results[0].picture.large)
-      setAge(data.results[0].dob.age)
-      setCity(data.results[0].location.city)
-      setState(data.results[0].location.state)
+      // setFirstName(data.results[0].name.first);
+      // setLastName(data.results[0].name.last);
+      // setAvatar(data.results[0].picture.large)
+      // setAge(data.results[0].dob.age)
+      // setCity(data.results[0].location.city)
+      // setState(data.results[0].location.state)
       })
       .catch(
         err => {
@@ -47,36 +47,35 @@ const Main = () => {
     return (
         <div className="main">
           <h1>Random User Generator</h1>
+          <div className="message">
           { isLoading && <div className="whileLoading">Loading....</div> }
           { errorMessage && <div className="whileLoading">{ errorMessage }</div> }
+          </div>
              <div className="profile">  
               {user &&  
               <div className="profile-card">
                   <div className="next">
-                    <h2>{firstName} {lastName}</h2>
+                    <h2>{user.name.first} {user.name.last}</h2>
                     <button onClick={fetchUser}>New User</button>
                   </div>
 
                   <div className="details">
-                      <img src={avatar} alt="Avatar"/> 
+                      <img src={user.picture.large} alt="Avatar"/> 
                       <div className="detailsContainer">
                         <div className="personal">
                           <h2>Personal Details</h2>
-                          {/* <hr/> */}
-                          <p>First Name: {firstName}</p>
-                          <p>Last Name: {lastName}</p>
-                          <p>Age: {age}</p>
-                          <p>City: {city}</p>
-                          <p>State: {state}</p>
+                          <p>First Name: {user.name.first}</p>
+                          <p>Last Name: {user.name.last}</p>
+                          <p>Age: {user.dob.age}</p>
+                          <p>Email: {user.email}</p>
                         </div> 
                         <div className="personal">
                           <h2>Other Details</h2>
-                          {/* <hr/> */}
-                          <p>First Name: {firstName}</p>
-                          <p>Last Name: {lastName}</p>
-                          <p>Age: {age}</p>
-                          <p>City: {city}</p>
-                          <p>State: {state}</p>
+                          <p>Phone No: {user.phone}</p>
+                          <p>Username: {user.login.username}</p>
+                          <p>City: {user.location.city}</p>
+                          <p>State: {user.location.state}</p>
+                          <p>Country: {user.location.country}</p>
                         </div>
                       </div>
                   </div>    
